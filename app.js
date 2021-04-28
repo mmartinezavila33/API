0cc55b2e3869cc0a2df4d5da3ce2116b5161cb1f
mmartinezavila33 <mmartinezavila33@correo.unicordoba.edu.co>
2021-04-28 12:35:27 -0500

    Summary
    CREACION DE UNA API BASICA

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+app.get('/', (req, res) => {
+    res.send('Bienvenidos!! Estoy feliz que Hagas Parte De Mi Api, Espero sea De su Agrado');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
+process.env.RUNKIT_ENDPOINT_URL;
