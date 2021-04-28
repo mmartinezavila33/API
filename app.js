1d442a9dcc3100361e5a743ba4e0241957a84b9f
mmartinezavila33 <mmartinezavila33@correo.unicordoba.edu.co>
2021-04-28 12:37:14 -0500

    Creación de API básica

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+
 const app = express();
+
 const port = 8080;
+
+
 app.get('/', (req, res) => {
+
+
     res.send('Bienvenidos!! Estoy feliz que Hagas Parte De Mi Api, Espero sea De su Agrado');
 })
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
